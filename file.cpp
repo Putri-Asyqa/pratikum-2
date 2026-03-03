a5b2eceb9eceaa94757a19a5b63378b752d9a814
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-03 20:39:41 +0700

    bagian fungsi

--- file.cpp
+++ file.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+
 //deklarasi variable 
 float jarijari;
 float phi = 3.14159;
+//bagian fungsi
+void awal(){
+    cout << "Masukkan jari-jari lingkaran = ";
+    cin >> jarijari;
+}
