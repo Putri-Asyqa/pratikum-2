5997033e53d014939784dc510202e393f5ba5418
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-03 20:45:21 +0700

    implementasi prosedur dan fungsi

--- file1.cpp
+++ file1.cpp
@@ -4,3 +4,14 @@ using namespace std;
 
 // deklarasi global
 int p, l, luas;
+
+//Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+    }
+int luasPersegi () {
+    return p * l;
+}
