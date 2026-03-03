e488549424858f684a93641ebe7b98963d3aec74
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-03 20:40:45 +0700

    fungsi proses

--- file.cpp
+++ file.cpp
@@ -9,3 +9,15 @@ void awal(){
     cout << "Masukkan jari-jari lingkaran = ";
     cin >> jarijari;
 }
+//fungsi proses
+float luas (float jari_jari, float phi){
+    return phi * jarijari * jarijari;
+}
+void akhir (){
+    cout << "Hasil luas untuk lingkaran dengan jari-jari " << jarijari <<" Adalah: " <<luas(jarijari, phi);
+
+}
+int main (){
+    awal();
+    akhir();
+}
